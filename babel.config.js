1fc1cd149138d38c35cc1b81d543069a37f39029
Joe Gornick <joe@joegornick.com>
2019-09-07 23:55:18 -0500

    chore: add jest babel and enzyme support

---
+++ babel.config.js
@@ -0,0 +1,35 @@
+const CONFIG_TYPES = [
+  'test',
+]
+
+module.exports = (api) => {
+  const env = api.env()
+
+  if (!CONFIG_TYPES.includes(env)) {
+    throw new Error(`${env} is not implemented! Try setting babel.envName.`)
+  }
+
+  api.cache.using(() => env)
+
+  const babelPresetEnvOptions = {
+    targets: {
+      esmodules: true
+    }
+  }
+
+  const config = {
+    retainLines: true,
+    presets: [
+      ['@babel/preset-env', babelPresetEnvOptions],
+      '@babel/preset-typescript',
+      '@babel/preset-react',
+    ],
+    plugins: [
+      // Needed to strip react-native flow typings
+      '@babel/plugin-transform-flow-strip-types',
+      '@babel/proposal-class-properties',
+    ],
+  }
+
+  return config
+}
