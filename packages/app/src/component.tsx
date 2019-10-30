7251f6f66bebf80f6b95bbd90f02d7e22ea79170
Joe Gornick <joe@joegornick.com>
2019-10-29 22:38:54 -0500

    fix(app): add text to indicate app component

--- packages/app/src/component.tsx
+++ packages/app/src/component.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
 import { HelloWorld } from '@jgornick/rnw-starter-hello-world'
+import { Text, View } from 'react-native'
 
-export const App = () => <HelloWorld />
+export const App = () => <View><Text>App</Text><HelloWorld /></View>
