68fe230dc11ac5cc08a40eccc20619832b5b8ce5
Joe Gornick <joe@joegornick.com>
2019-10-29 22:39:48 -0500

    fix(native): upgrade to latest haul and babel

--- packages/native/haul.config.js
+++ packages/native/haul.config.js
@@ -1,43 +1,10 @@
-import { makeConfig } from "@haul-bundler/preset-0.60";
-import { find, get, isString, some } from 'lodash'
+import { makeConfig } from '@haul-bundler/preset-0.60'
 import path from 'path'
 import resolvePkg from 'resolve-pkg'
 
-Object.defineProperty(RegExp.prototype, "toJSON", {
+Object.defineProperty(RegExp.prototype, 'toJSON', {
   value: RegExp.prototype.toString
 });
-
-function findRuleByLoader(config, loaderName) {
-  return find(config.module.rules, r => {
-    return some(
-      r.use,
-      u => isString(u)
-        ? u.indexOf(loaderName) > -1
-        : u.loader.indexOf(loaderName) > -1
-    )
-  })
-}
-
-function findLoaderInRule(rule, loaderName) {
-  return find(
-    get(rule, 'use', []),
-    u => isString(u)
-      ? u.indexOf(loaderName) > -1
-      : u.loader.indexOf(loaderName) > -1
-  )
-}
-
-function setupBabelRule(rule) {
-  const babelLoader = findLoaderInRule(rule, 'babel-loader')
-
-  // If we found the rule, replace it's loader options,
-  // so `babel-loader` will require our own custom `babel.config.js`
-  babelLoader.options = {
-    envName: 'native',
-    rootMode: 'upward',
-    cacheDirectory: true,
-  }
-}
 
 export default makeConfig({
   bundles: {
@@ -56,9 +23,6 @@ export default makeConfig({
           resolvePkg('react-native/Libraries/Core/InitializeCore.js'),
           path.resolve(__dirname, 'index.ts'),
         ]
-
-        const babelRule = findRuleByLoader(config, 'babel-loader')
-        setupBabelRule(babelRule)
 
         runtime.logger.info(JSON.stringify(config, null, '  '))
 
--- packages/native/index.ts
+++ packages/native/index.ts
@@ -1,22 +1,23 @@
-import { getStorybookUI, configure } from '@storybook/react-native';
-import { AppRegistry } from 'react-native';
+import { getStorybookUI, configure } from '@storybook/react-native'
+import { AppRegistry } from 'react-native'
 
-import { name as appName } from './app.json';
+import { name as appName } from './app.json'
 
 function loadStories() {
   return [
+    require('../app/src/component.story'),
     require('../hello-world/src/component.story')
   ]
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
+const StorybookUIRoot = getStorybookUI({})
 
 // If you are using React Native vanilla write your app name here.
 // If you use Expo you can safely remove this line.
-AppRegistry.registerComponent(appName, () => StorybookUIRoot);
+AppRegistry.registerComponent(appName, () => StorybookUIRoot)
 
-export default StorybookUIRoot;
+export default StorybookUIRoot
