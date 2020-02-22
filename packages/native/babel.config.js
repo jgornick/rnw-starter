b9903d6673da389276e4d133c3df7cb7d148da0b
Joe Gornick <joe@joegornick.com>
2020-02-22 10:40:14 -0600

    fix(native): remove hermes so app can run without debug on

--- packages/native/babel.config.js
+++ packages/native/babel.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  presets: ['module:@haul-bundler/babel-preset-react-native'],
+  presets: [
+    ['module:@haul-bundler/babel-preset-react-native']
+  ],
 };
--- packages/native/index.tsx
+++ packages/native/index.tsx
@@ -16,8 +16,6 @@ configure(loadStories, module)
 // To find allowed options for getStorybookUI
 const StorybookUIRoot = getStorybookUI({})
 
-// If you are using React Native vanilla write your app name here.
-// If you use Expo you can safely remove this line.
 AppRegistry.registerComponent(appName, () => StorybookUIRoot)
 
 export default StorybookUIRoot
